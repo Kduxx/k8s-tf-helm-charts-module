3f02b3cebd3b3512c5117f212a77ca080195934e
Kduxx <carlosedu.dev@gmail.com>
2022-06-01 23:12:51 -0300

    Chore: remove local provider configuration

--- providers.tf
+++ providers.tf
@@ -6,12 +6,3 @@ terraform {
     }
   }
 }
-
-provider "helm" {
-  kubernetes {
-    host                   = var.k8s_master_endpoint
-    cluster_ca_certificate = base64decode(var.k8s_master_Ca)
-    token                  = var.k8s_master_auth_token
-
-  }
-}
